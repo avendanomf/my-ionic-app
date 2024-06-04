257961bfb7d8eaa671bd480c5cb28a3dc2250674
avendanomf <mfavendano-bermudez@outlook.com>
2024-06-03 19:25:14 -0500

    Ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyADBj5eIfw7QbXZ108QswSjE07xMmamZ2Q",
+    authDomain: "calculo-cho-app.firebaseapp.com",
+    projectId: "calculo-cho-app",
+    storageBucket: "calculo-cho-app.appspot.com",
+    messagingSenderId: "849439955911",
+    appId: "1:849439955911:web:f8778c7db77922531417e2"
+  }
 };
 
 /*
